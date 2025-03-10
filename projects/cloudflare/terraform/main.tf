c1e4e9b22936b3cce8f1e4338b3d1621c03bafc8
Yunior Alayo <yunior.alayo@gmail.com>
2025-03-10 11:19:07 +0100

    Fixing some terraform small issues.

--- projects/cloudflare/terraform/main.tf
+++ projects/cloudflare/terraform/main.tf
@@ -25,11 +25,11 @@ provider "cloudflare" {
 }
 
 resource "cloudflare_workers_script" "score_backend" {
-  account_id = "4180cebb0f75e57d434f27a622da70f0"
-  name       = "mtd-app"
+  account_id  = "4180cebb0f75e57d434f27a622da70f0"
+  script_name = "mtd-app"
   compatibility_date = "2024-01-01"
-  module     = true
-  content    = file("main.js")
+  has_modules = true
+  content     = file("main.js")
   bindings = [{
     name = "DB"
     type = "d1"
